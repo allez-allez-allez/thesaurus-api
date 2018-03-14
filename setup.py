2338b768a79c42b5e5ae2a3cbc3e6e91ed119ba2
rahilwazir <rahilwazir@hotmail.com>
2018-03-15 00:46:54 +0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='thesaurus',
+    version='0.1',
+    py_modules=['thesaurus'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ]
+)
